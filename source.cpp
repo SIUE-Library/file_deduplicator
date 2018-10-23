e8641960fa446ed75fc05160b5c54d2038d495ec
Jacob Grubb <jgrubb013@gmail.com>
2018-10-23 16:20:59 -0500

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,8 @@
+/* Project: File Deduplicator
+ * Organization: The Lovejoy Library of Southern Illinois University Edwardsville
+ * Department: Digital Initiatives and
+ * Programmer: Jacob Grubb
+ * File: source.cpp
+*/
+
+//
