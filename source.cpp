db67e558c1e1b1783df5be18ffa55e7ec7dd4198
Jacob Grubb <jgrubb013@gmail.com>
2018-10-24 11:18:24 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -5,4 +5,24 @@
  * File: source.cpp
 */
 
+/*
+struct fileHas
+{
+  string fileName
+  
+}
+
+*/
+
+//Get the List of Files
 //
+
+//Create a hash of every file
+
+//Create structs for each file of {pathToFile, hash}
+
+//Sort the list of structs by hash
+
+//Identify segments of duplicates
+
+//Print duplicate segments
