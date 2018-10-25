1f2cf29ddee8aa96e88e1c17eae1cf2d3c5cf198
Jacob Grubb <jgrubb013@gmail.com>
2018-10-25 16:33:52 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -9,15 +9,17 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 //Function Prototyping
 int main(int argc, char* argv[]);
-int makeHash(string fileName, vector<fileHash> fileHashArr);
+int makeHash(string fileName);
+void checkFolder(string folderName, vector<fileHash> *fileHashArr);
 
 struct fileHash
 {
   string fileName
-  int fileHash
+  int hashCode
 } ;
 
 
@@ -26,7 +28,7 @@ int main(int argc, char* argv[])
   if(argc != 2)
   {
     std::cerr << "Invalid Parameter. \n Correct syntax: ./deduplicate.out <root directory>";
-    exit(-1) 
+    exit(-1) ;
   }
   
   
@@ -35,17 +37,21 @@ int main(int argc, char* argv[])
   //Get the List of Files
   //  -Recursively, perhaps?
   //    checkfolder(folder)
-  checkFolder(
+  checkFolder(argv[1], &fileHashArr);
   //Create a hash of every file
   //Create structs for each file of {pathToFile, hash}
 
   //Sort the list of structs by hash
+  std::sort(fileHashArr.begin(), fileHashArr.end(), [](const fileHash& h1, const fileHas &h2)
+	{
+		return h1.hashCode < h2.hashCode;
+	});
   
   //Print duplicate segments
   for(int i = 1; i < fileHashArr.size(); i++)
   {
     int j = i + 1;
-    while(j < fileHashArr.size() && fileHashArr[i].fileHash == fileHashArr[j].fileHash)
+    while(j < fileHashArr.size() && fileHashArr[i].hashCode == fileHashArr[j].hashCode)
     {
       //Add fileHashArr[j] to the list
       j++; 
@@ -57,11 +63,10 @@ int main(int argc, char* argv[])
 
 int makeHash(string fileName)
 {
-  
   return 0;
 }
 
-void checkFolder(string folderName, vector<fileHash> fileHashArr)
+void checkFolder(string folderName, vector<fileHash>* fileHashArr)
 {
   //      for each subfolder in folder
   //        checkfolder(subfolder)
